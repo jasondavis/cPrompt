d086ec77f79ba7ddeb7798c03aae238c4b2f9606
Michael Wright <michael@wserver.co.uk>
2012-05-01 19:23:12 +0100

    Commit First Version

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ cPrompt
 =======
 
 A simple drop-in JS script that will prompt your users to allow cookies on your site. As per the new EU law released on the 26th May.
+
+**A brain child of @panda_doodle, with @michaelw90 coding it.**
+
+Demo
+=====
+
+A demo can be seen [here](http://michaelwright.me/cPrompt/demo.html).
---
+++ src/cprompt.js
@@ -0,0 +1,52 @@
+/**
+	Simple Cookie Prompt
+	Idea: @panda_doodle - Coded: @michaelw90
+**/
+var cPrompt = {
+
+	cookie: false,
+
+	init: function(){
+		if(!this.checkCookie()){
+			this.loadPrompt();
+		}
+	},
+
+	checkCookie: function(){
+		if(document.cookie.match(/cPrompt_useCookies=1/)){
+			this.cookie = true;
+		}
+		return this.cookie;
+	},
+
+	loadPrompt: function(){
+		var holder = document.createElement('div');
+		holder.innerHTML = "<div style='margin: 10px;'>This site would like to place cookies on your computer to help make this website better. To find out more about the cookies, visit <a href='en.wikipedia.org/wiki/HTTP_cookie' style='color: #FFF;'>Wikipedia</a>.<br /><input type='checkbox' onclick='cPrompt.clickEvent(this);'> I accept cookies from this site.</div>"
+		holder.style.cssText = 'background: #000; position: absolute; width: 100%; color: #FFF; font-family: verdana; font-size: 11px; opacity: 0.85';
+		holder.id = 'cPrompt_Holder';
+		var b = document.getElementsByTagName('body')[0];
+		b.insertBefore(holder, b.firstChild);
+	},
+
+	saveCookie: function(){
+		document.cookie = "cPrompt_useCookies=1;expires=" + (new Date()).toGMTString().replace(/\d{4}/, '2050');
+	},
+
+	clickEvent: function(obj){
+		if(obj.checked){
+			this.saveCookie();
+			document.getElementById('cPrompt_Holder').style.display = 'none';
+		}
+	}
+}
+if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		cPrompt.init();
+	}, false);
+}else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		if(document.readyState === "complete"){
+			cPrompt.init();
+		}
+	});
+}
